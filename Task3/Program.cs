f51fbbc5a7d13e6a0dcfca9e6c6e3612e7f96c3b
Daniil <daniilkukotsky@gmail.com>
2023-07-17 00:52:54 +0400

    First and final commit Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿void kub(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+    int num1 = i * i * i;
+    Console.WriteLine(num1 + " ");
+    }
+}
+
+Console.Write("Enter your number:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+kub(num1);
